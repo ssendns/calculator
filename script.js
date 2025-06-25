e72e5a62af416e98d9eabd0f2188644ed0ed8456
Anna Parilova <187332467+ssendns@users.noreply.github.com>
2025-06-25 12:24:05 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+const add = function(a, b) {
+	return a + b;
+};
+
+const subtract = function(a, b) {
+	return a - b;
+};
+
+const multiply = function(a, b) {
+  return a * b;
+};
+
+const divide = function(a, b) {
+    return a / b;
+};
+
+const operate = function(operator, a, b) {
+    switch (operator) {
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case '*':
+            return multiply(a, b);
+        case '/':
+            if (b === 0) {
+                return "Cannot divide by zero";
+            }
+            return divide(a, b);
+        default:
+            return "error";
+    }
+};
